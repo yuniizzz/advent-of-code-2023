6862a4dc4d360db1deb6bb3a9d94d4049ef6a2e4
채종윤 <yuniithings@gmail.com>
2023-12-10 01:27:25 +0900

    add: day별 input 구해오는 fetch 함수 추가

---
+++ typescript/fetch.ts
@@ -0,0 +1,12 @@
+import { AR, S, pipe } from '@mobily/ts-belt'
+
+export const fetchInput = (session: string) => (day: number) => {
+  return pipe(
+    fetch(`https://adventofcode.com/2023/day/${day}/input`, {
+      headers: {
+        cookie: `session=${session}`
+      }
+    }).then(res => res.text()).then(S.trim),
+    AR.make,
+  );
+}
